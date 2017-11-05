31d424ee86ce83b35bce779a01b2bbd8eca0a64c
Pimentel <apimentela@iingen.unam.mx>
2017-11-05 12:45:23 -0600

    agregué comentarios al código y subí un archivo de salida con todos los patrones de 'en' que salieron de todos los textos, también subí el doc que me pasaron con patrones

--- pre.py
+++ pre.py
@@ -4,25 +4,41 @@
 import re
 
 def main(args):
+	"""
+	Esta sección del código simplemente lee el texto
+	"""
 	s_nombre_archivo=args[1]
 	with open(s_nombre_archivo,"r") as archivo:
 		s_texto_original=archivo.read()
 	
-	expresion_derecha=re.compile(r"([^\w\s][^A-Z\d])")
-	expresion_izquierda=re.compile(r"([^A-Z\d][^\w\s])")
-	s_texto=expresion_derecha.sub(r" \1",s_texto_original)
-	s_texto=expresion_izquierda.sub(r"\1 ",s_texto)
-	s_texto=expresion_derecha.sub(r" \1",s_texto)
-	
-	expresion_media=re.compile(r"(?<! )[^\w\s](?! )")
-	s_texto=expresion_media.sub("",s_texto)
-	
-	expresion_en=re.compile(r"\ben [\w\s]*")
+	"""
+	Aquí pongo unas cuantas expresiones regulares para quitar símbolos que estén separando:
+		siglas, números, direcciones web o cualquier otra cosa,
+		para que sea más fácil hacer pruebas sin tener que tomar eso en cuenta,
+		esa medida es MOMENTANEA, más adelante hay que trabajar en las expresiones con todo detalle
+		y quitar las de este bloque, porque estas modifican el texto original
+		y es preferible no hacer eso al final
+	"""
+	expresion_derecha=re.compile(r"([^\w\s][^A-Z\d])")		# La expresión está separada en dos partes, una para cada lado de los signos de puntuación
+	expresion_izquierda=re.compile(r"([^A-Z\d][^\w\s])")	# la primera para el lado derecho, y esta para el lado izquierdo
+	s_texto=expresion_derecha.sub(r" \1",s_texto_original)	# se reemplaza, según el lado, por espacio
+	s_texto=expresion_izquierda.sub(r"\1 ",s_texto)			# para que todo lo que no está entre mayúsculas o números se separe entre espacios y sea más fácil de tratar
+	s_texto=expresion_derecha.sub(r" \1",s_texto)			# esto se hace dos veces, por si el segundo afectó el texto de modo que se limpie por completo.
+	expresion_media=re.compile(r"(?<! )[^\w\s](?! )")		# esta expresión buscará todos los símbolos que no fueron separados con espacios
+	s_texto=expresion_media.sub("",s_texto)					# para eliminarlos y que el texto quede como si las entidades que separaban fueran una sola.
+	"""
+	Aquí comienzan los patrones de verdad
+	"""
+	expresion_en=re.compile(r"\ben\b[\w\s]*")
 	resultados_en=expresion_en.finditer(s_texto)
 	for resultado in resultados_en:
 		print(resultado.group(0))
 	return 0
 
 if __name__ == '__main__':
+	"""
+	Se debe ejecutar el código con un elemento de entrada:
+		- el nombre de el archivo que se va a atrabajar
+	"""
 	import sys
 	sys.exit(main(sys.argv))
