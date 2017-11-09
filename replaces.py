1c5f03aaf240f313f12372b611f62613caedab06
Pimentel <apimentela@iingen.unam.mx>
2017-11-09 11:27:26 -0600

    # TODO : podríamos, como dicen, filtrar: Messenger, Twitter, Facebook, Internet, Instagram, Google, Skype, Tumblr, Pocket, Telegram, Pinterest, Reddit, Linkedin, Youtube, Síguenos, Inicio # TODO : al parecer, aún se puede filtrar mucho si se fijan en las palabras funcionales en los extremos de las supuestas ubicaciones, además, hay que buscar cosas parecidas a "la cual, el cual, el que..." que tampoco dan buenos resultados, y se necesita también una lista de idiomas, por cosas como "en Español..." y cosas como "en HD, en PDF, en la televisión,...", y categorías de periódicos. Además de colocaciones como "en buscda de...." # TODO : verificar expresiones de tiempo como "en el siglo.." # TODO : verificar tambián partes del cuerpo "en el pecho.." # TODO : entidades con mayúsculas pueden tener palabras con minúsculas en medio, aunque no he encontrado que haya más de dos, revisar expresión regular. # TODO : hay que analizar lo de las palabras funcional en los extremos de las entidades, muchas veces solo marcan el final, no que no lo sea. (quitar "en" de esas palabras y agregar "leer", y quizá se tenga que revisar verbos en general, palabras como "te" parecen ser terminantes) # TODO : varias entidades que van en mayúsculas y minúsculas si terminan o comienzan con UNA sigla en puras mayúsculas, revisar expresión regular. *revisar que no sea peor el resultado, hay siglas que son seguidas de mayúsculas pero no tienen nada que ver # TODO : algunos lugares son seguidos de un lugar más general en paréntesis. (no son tan comúnes)

--- replaces.py
+++ replaces.py
@@ -6,5 +6,5 @@ import re
 def ne00u00(matchobj):
 	texto_encontrado = matchobj.group(0).strip()
 	texto_nuevo = texto_encontrado.replace(" ","_")
-	texto_nuevo = "NE00C00_"+texto_nuevo+" "
+	texto_nuevo = " "+"NE00C00_"+texto_nuevo+" "
 	return texto_nuevo
