35e1a51f6242ec710cb25c23246942d352782d14
Pimentel <apimentela@iingen.unam.mx>
2017-11-13 21:14:02 -0600

    ajustes para primera entrega

--- no_server.py
+++ no_server.py
@@ -8,9 +8,6 @@ def main(args):
     """
     Se pasa como parámetro el archivo que queremos analizar con freeling.
     La salida es un archivo llamado file_out.txt
-    Asumí que freeling está a través de Dockers escuchando en el puerto 50005, ip 172.17.0.2
-    Este es el comando para levantar el servidor:
-    docker run -it --rm -p 50005:50005 freeling analyze -f es.cfg --outlv tagged --ner --nec --date --server -p 50005
     """
     s_nombre_archivo = args[1]
     with open(s_nombre_archivo,"r",encoding="utf-8") as entrada:
