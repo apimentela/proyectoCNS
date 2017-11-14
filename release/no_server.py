c70d358ce78e1a133d01de2e176891b2f0e296d5
Pimentel <apimentela@iingen.unam.mx>
2017-11-13 21:25:21 -0600

    ajustes para primera entrega

--- release/no_server.py
+++ release/no_server.py
@@ -1,29 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#Archivo para llamar a freeling
+y=open
+F=True
 import subprocess
+c=subprocess.call
 import re
-
-def main(args):
-    """
-    Se pasa como parámetro el archivo que queremos analizar con freeling.
-    La salida es un archivo llamado file_out.txt
-    Asumí que freeling está a través de Dockers escuchando en el puerto 50005, ip 172.17.0.2
-    Este es el comando para levantar el servidor:
-    docker run -it --rm -p 50005:50005 freeling analyze -f es.cfg --outlv tagged --ner --nec --date --server -p 50005
-    """
-    s_nombre_archivo = args[1]
-    with open(s_nombre_archivo,"r",encoding="utf-8") as entrada:
-        #no lo metí en otro with open porque daba error. Así que lo dejo así.
-        salida = open("resultadoFreeling.flg", "a+",encoding="utf-8")
-        subprocess.call("C:\\freeling\\bin\\analyzer.bat -f es.cfg", stdin=entrada, stdout=salida, shell=True)
-        salida.close()
-    return 0
-
-if __name__ == '__main__':
-    """
-    Se debe ejecutar el código con un elemento de entrada:
-        - el nombre de el archivo que se va a atrabajar
-    """
-    import sys
-    sys.exit(main(sys.argv))
+def p(args):
+ s=args[1]
+ with y(s,"r",encoding="utf-8")as entrada:
+  W=y("resultadoFreeling.flg","a+",encoding="utf-8")
+  c("C:\\freeling\\bin\\analyzer.bat -f es.cfg",stdin=entrada,stdout=W,shell=F)
+  W.close()
+ return 0
+if __name__=='__main__':
+ import sys
+ sys.exit(p(sys.argv))
