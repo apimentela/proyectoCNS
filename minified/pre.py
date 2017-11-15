8778ef6771118de48c428c742ef03fc9af21f229
Pimentel <apimentela@iingen.unam.mx>
2017-11-14 21:54:35 -0600

    nuevo release... NO ESTA PROBADO

---
+++ minified/pre.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+IP=open
+at=IP
+import re
+It=re.M
+IF=re.compile
+aQ=IF
+aH=It
+import pickle
+IK=pickle.dump
+al=IK
+a=["NE00C00","NE00P00","NE00O00","NE00T00","NE00I00","NE00E00","NE00M00","NE00A00","NE00L00","NE00S00","NE00U00","NE00W00"]
+def Iz(matchobj):
+ for s in a:
+  if s in matchobj.group(0):
+   return matchobj.group(0)
+ o=matchobj.group(0).strip()
+ G=o.replace(" ","_")
+ G=" "+"NE00C00_"+G+" "
+ return G
+def Iw(matchobj):
+ for s in a:
+  if s in matchobj.group(0):
+   return matchobj.group(0)
+ o=matchobj.group(0).strip()
+ G=o.replace(" ","_")
+ G=" "+"NE00P00_"+G+" "
+ return G
+def Iu(matchobj):
+ for s in a:
+  if s in matchobj.group(0):
+   return matchobj.group(0)
+ o=matchobj.group(0).strip()
+ G=o.replace(" ","_")
+ G=" "+"NE00O00_"+G+" "
+ return G
+def Io(matchobj):
+ for s in a:
+  if s in matchobj.group(0):
+   return matchobj.group(0)
+ o=matchobj.group(0).strip()
+ G=o.replace(" ","_")
+ G=" "+"NE00T00_"+G+" "
+ return G
+def IX(matchobj):
+ for s in a:
+  if s in matchobj.group(0):
+   return matchobj.group(0)
+ o=matchobj.group(0).strip()
+ G=o.replace(" ","_")
+ G=" "+"NE00I00_"+G+" "
+ return G
+def IG(matchobj):
+ for s in a:
+  if s in matchobj.group(0):
+   return matchobj.group(0)
+ G=" "+"NE00E00"+" "
+ return G
+def Ik(matchobj):
+ for s in a:
+  if s in matchobj.group(0):
+   return matchobj.group(0)
+ o=matchobj.group(0).strip()
+ G=o.replace(" ","_")
+ G=" "+"NE00M00_"+G+" "
+ return G
+def Im(matchobj):
+ for s in a:
+  if s in matchobj.group(0):
+   return matchobj.group(0)
+ o=matchobj.group(0).strip()
+ G=o.replace(" ","_")
+ G=" "+"NE00A00_"+G+" "
+ return G
+def IM(matchobj):
+ for s in a:
+  if s in matchobj.group(0):
+   return matchobj.group(0)
+ o=matchobj.group(0).strip()
+ G=o.replace(" ","_")
+ G=" "+"NE00W00_"+G+" "
+ return G
+def Ij(matchobj):
+ for s in a:
+  if s in matchobj.group(0):
+   return matchobj.group(0)
+ o=matchobj.group(0).strip()
+ G=o.replace(" ","_")
+ G=" "+"NE00S00_"+G+" "
+ return G
+def IC(matchobj):
+ for s in a:
+  if s in matchobj.group(0):
+   return matchobj.group(0)
+ o=matchobj.group(0).strip()
+ c=["Messenger","Twitter","Facebook","Internet","Instagram","Google","Skype","Tumblr","Pocket","Telegram","Pinterest","Reddit","Linkedin","Youtube","Síguenos","Inicio"]
+ for Q in c:
+  if o.startswith(Q):
+   return matchobj.group(0)
+ G=o.replace(" ","_")
+ G=" "+"NE00U00_"+G+" "
+ return G
+def IL(args):
+ H=r"\b(el|la|los|las|un|uno|una|unos|unas|lo|al|del)\b"
+ l=r"\b(el|la|los|las|lo|al|del)\b"
+ t=r"\b(mi|mis|nuestro|nuestros|nuestra|nuestras|tu|tus|vuestro|vuestros|vuestra|vuestras|su|sus)\b"
+ E=r"\b(mi|mis|nuestro|nuestros|nuestra|nuestras)\b"
+ W=r"\b[Ee]st(oy|ás|á|amos|áis|án|aba|abas|aba|ábamos|abais|aban|uve|uviste|uvo|uvimos|uvisteis|uvieron|aré|arás|ará|aremos|aréis|arán|aría|arías|aríamos|arían|é|és|emos|éis|én|uviera|uvieras|uviéramos|uvierais|uvieran|uviese|uvieses|uviese|uviésemos|uvieseis|uviesen|uviere|uvieres|uviere|uviéremos|uviereis|uvieren)\b"
+ J=r"\b[hH](e|as|a|emos|abéis|an|abía|abías|abía|abíamos|abíais|abían|ube|ubiste|ubo|ubimos|ubisteis|ubieron|abré|abrás|abrá|abremos|abréis|abrán|abría|abrías|abríamos|abríais|abrían|aya|ayas|ayamos|ayáis|ayan|ubiera|ubieras|ubiéramos|ubierais|ubieran|ubiese|ubieses|ubiésemos|ubieseis|ubiesen|ubiere|ubieres|ubiéremos|ubiereis|ubieren) +estado\b"
+ e=r"\band(o|as|a|amos|áis|an|aba|abas|aba|ábamos|abais|aban|uve|uviste|uvo|uvimos|uvisteis|uvieron|aré|arás|ará|aremos|aréis|arán|aría|arías|aríamos|arían|e|es|emos|éis|en|uviera|uvieras|uviéramos|uvierais|uvieran|uviese|uvieses|uviese|uviésemos|uvieseis|uviesen|uviere|uvieres|uviere|uviéremos|uviereis|uvieren)\b"
+ C=r"\b(he|has|ha|hemos|habéis|han|había|habías|había|habíamos|habíais|habían|hube|hubiste|hubo|hubimos|hubisteis|hubieron|habré|habrás|habrá|habremos|habréis|habrán|habría|habrías|habríamos|habríais|habrían|haya|hayas|hayamos|hayáis|hayan|hubiera|hubieras|hubiéramos|hubierais|hubieran|hubiese|hubieses|hubiésemos|hubieseis|hubiesen|hubiere|hubieres|hubiéremos|hubiereis|hubieren) +andado\b"
+ z=r"\bv(oy|as|a|amos|ais|an|aya|ayas|ayamos|ayáis|ayan)|\b(i|í)(ba|bas|bamos|bais|ban|ré|rás|rá|remos|réis|rán|ría|rías|ríamos|ríais|rían)|\bfu(i|iste|e|imos|isteis|eron|era|eras|éramos|erais|eran|ese|eses|ésemos|eseis|esen|ere|eres|éremos|ereis|eren)\b"
+ I=r"\b(he|has|ha|hemos|habéis|han|había|habías|había|habíamos|habíais|habían|hube|hubiste|hubo|hubimos|hubisteis|hubieron|habré|habrás|habrá|habremos|habréis|habrán|habría|habrías|habríamos|habríais|habrían|haya|hayas|hayamos|hayáis|hayan|hubiera|hubieras|hubiéramos|hubierais|hubieran|hubiese|hubieses|hubiésemos|hubieseis|hubiesen|hubiere|hubieres|hubiéremos|hubiereis|hubieren) +ido\b"
+ y=r"\bviaj(o|as|a|amos|áis|an|aba|abas|aba|ábamos|abais|aban|é|aste|ó|amos|asteis|aron|aré|arás|ará|aremos|aréis|arán|aría|arías|aríamos|arían|e|es|emos|éis|en|ara|aras|áramos|arais|aran|ase|ases|ásemos|aseis|asen|are|ares|áremos|areis|aren)\b"
+ w=r"\b(he|has|ha|hemos|habéis|han|había|habías|había|habíamos|habíais|habían|hube|hubiste|hubo|hubimos|hubisteis|hubieron|habré|habrás|habrá|habremos|habréis|habrán|habría|habrías|habríamos|habríais|habrían|haya|hayas|hayamos|hayáis|hayan|hubiera|hubieras|hubiéramos|hubierais|hubieran|hubiese|hubieses|hubiésemos|hubieseis|hubiesen|hubiere|hubieres|hubiéremos|hubiereis|hubieren) +viajado\b"
+ v=args[1]
+ with at(v,"r",encoding="utf-8")as archivo:
+  D=archivo.read()
+ r=D
+ j=l+r"( +[^A-ZÁÉÍÓÚÑ\W]+ +)*?"
+ O=r"(([A-ZÁÉÍÓÚÑ]+[ \b]+)+|([A-ZÁÉÍÓÚÑ][^A-ZÁÉÍÓÚÑ\W]+[ \b]+)+)"
+ S=aQ(r"(?<=\b[Ee]n )+("+j+O+"|"+O+")")
+ r=S.sub(IC,r)
+ Y=aQ(r"("+W+"|"+J+")"+" +entre +[\w ]+")
+ B=aQ(r"("+W+"|"+J+")"+" +(cerca|lejos) +de +[\w ]+")
+ f=r"[A-HJ-NPR-Z]{3}-?[0-9]{2}-?[0-9]{2}"
+ U=r"[A-HJ-NPR-Z]{3}-?[0-9]{3}-?[A-HJ-NPR-Z]"
+ L=r"[A-HJ-NPR-Z]-?[0-9]{2,3}-?[A-HJ-NPR-Z]{3}"
+ V=r"[0-9]{3}-?[A-HJ-NPR-Z]{3}"
+ x=r"[A-HJ-NPR-Z]{2}-?[0-9]{2}-?[0-9]{3}"
+ p=r"[0-9]-?[A-HJ-NPR-Z]-?[0-9]{2}-?[A-HJ-NPR-Z]{2}"
+ P=r"[0-9]-?[A-HJ-NPR-Z]-?[0-9]-?[A-HJ-NPR-Z]{3}"
+ X=aQ(r"\b("+f+"|"+U+"|"+L+"|"+V+"|"+x+"|"+p+"|"+P+")\b")
+ r=X.sub(Ik,r)
+ k=aQ(r"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"+r'"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])')
+ T=k.finditer(r)
+ h=[]
+ for n in T:
+  h.append(n.group(0))
+ with at("lista_correos.pkl","wb")as f:
+  al(h,f)
+ r=k.sub(IG,r)
+ m=aQ(r"^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$",aH)
+ r=m.sub(IX,r)
+ u=aQ(r"\b(\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4})\b")
+ r=u.sub(Io,r)
+ R=aQ(r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+")
+ r=R.sub(IM,r)
+ F=at("resultadoPre.flg","a+",encoding="utf-8")
+ F.write(r)
+ F.close()
+ return 0
+if __name__=='__main__':
+ import sys
+ sys.exit(IL(sys.argv))
+# Created by pyminifier (https://github.com/liftoff/pyminifier)
--- release/pre.py
+++ release/pre.py
@@ -1,152 +1,170 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-at=open
+jf=open
 import re
-aQ=re.compile
-aH=re.M
+jn=re.compile
+je=re.M
 import pickle
-al=pickle.dump
-a=["NE00C00","NE00P00","NE00O00","NE00T00","NE00I00","NE00E00","NE00M00","NE00A00","NE00L00","NE00S00","NE00U00","NE00W00"]
-def g(matchobj):
- for s in a:
-  if s in matchobj.group(0):
-   return matchobj.group(0)
- o=matchobj.group(0).strip()
- G=o.replace(" ","_")
- G=" "+"NE00C00_"+G+" "
- return G
-def b(matchobj):
- for s in a:
-  if s in matchobj.group(0):
-   return matchobj.group(0)
- o=matchobj.group(0).strip()
- G=o.replace(" ","_")
- G=" "+"NE00P00_"+G+" "
- return G
+jx=pickle.dump
+j=["NE00C00","NE00P00","NE00O00","NE00T00","NE00I00","NE00E00","NE00M00","NE00A00","NE00L00","NE00S00","NE00U00","NE00W00"]
+W={"NE00C00":[],"NE00P00":[],"NE00O00":[],"NE00T00":[],"NE00I00":[],"NE00E00":[],"NE00M00":[],"NE00A00":[],"NE00L00":[],"NE00S00":[],"NE00U00":[],"NE00W00":[]}
 def d(matchobj):
- for s in a:
-  if s in matchobj.group(0):
+ for z in j:
+  if z in matchobj.group(0):
    return matchobj.group(0)
- o=matchobj.group(0).strip()
- G=o.replace(" ","_")
- G=" "+"NE00O00_"+G+" "
- return G
-def q(matchobj):
- for s in a:
-  if s in matchobj.group(0):
+ w=matchobj.group(0).strip()
+ t=w.replace(" ","_")
+ W["NE00C00"].append(t)
+ t=" "+"NE00C00"+" "
+ return t
+def F(matchobj):
+ for z in j:
+  if z in matchobj.group(0):
    return matchobj.group(0)
- o=matchobj.group(0).strip()
- G=o.replace(" ","_")
- G=" "+"NE00T00_"+G+" "
- return G
+ w=matchobj.group(0).strip()
+ t=w.replace(" ","_")
+ W["NE00P00"].append(t)
+ t=" "+"NE00P00"+" "
+ return t
+def E(matchobj):
+ for z in j:
+  if z in matchobj.group(0):
+   return matchobj.group(0)
+ w=matchobj.group(0).strip()
+ t=w.replace(" ","_")
+ W["NE00O00"].append(t)
+ t=" "+"NE00O00"+" "
+ return t
 def M(matchobj):
- for s in a:
-  if s in matchobj.group(0):
+ for z in j:
+  if z in matchobj.group(0):
    return matchobj.group(0)
- o=matchobj.group(0).strip()
- G=o.replace(" ","_")
- G=" "+"NE00I00_"+G+" "
- return G
-def K(matchobj):
- for s in a:
-  if s in matchobj.group(0):
+ w=matchobj.group(0).strip()
+ t=w.replace(" ","_")
+ W["NE00T00"].append(t)
+ t=" "+"NE00T00"+" "
+ return t
+def b(matchobj):
+ for z in j:
+  if z in matchobj.group(0):
    return matchobj.group(0)
- G=" "+"NE00E00"+" "
- return G
-def i(matchobj):
- for s in a:
-  if s in matchobj.group(0):
+ w=matchobj.group(0).strip()
+ t=w.replace(" ","_")
+ W["NE00I00"].append(t)
+ t=" "+"NE00I00"+" "
+ return t
+def H(matchobj):
+ for z in j:
+  if z in matchobj.group(0):
    return matchobj.group(0)
- o=matchobj.group(0).strip()
- G=o.replace(" ","_")
- G=" "+"NE00M00_"+G+" "
- return G
-def N(matchobj):
- for s in a:
-  if s in matchobj.group(0):
+ w=matchobj.group(0).strip()
+ t=w.replace(" ","_")
+ W["NE00E00"].append(t)
+ t=" "+"NE00E00"+" "
+ return t
+def jW(matchobj):
+ for z in j:
+  if z in matchobj.group(0):
    return matchobj.group(0)
- o=matchobj.group(0).strip()
- G=o.replace(" ","_")
- G=" "+"NE00A00_"+G+" "
- return G
-def A(matchobj):
- for s in a:
-  if s in matchobj.group(0):
+ w=matchobj.group(0).strip()
+ t=w.replace(" ","_")
+ W["NE00M00"].append(t)
+ t=" "+"NE00M00"+" "
+ return t
+def jz(matchobj):
+ for z in j:
+  if z in matchobj.group(0):
    return matchobj.group(0)
- o=matchobj.group(0).strip()
- G=o.replace(" ","_")
- G=" "+"NE00W00_"+G+" "
- return G
-def ao(matchobj):
- for s in a:
-  if s in matchobj.group(0):
+ w=matchobj.group(0).strip()
+ t=w.replace(" ","_")
+ W["NE00A00"].append(t)
+ t=" "+"NE00A00"+" "
+ return t
+def jw(matchobj):
+ for z in j:
+  if z in matchobj.group(0):
    return matchobj.group(0)
- o=matchobj.group(0).strip()
- G=o.replace(" ","_")
- G=" "+"NE00S00_"+G+" "
- return G
-def aG(matchobj):
- for s in a:
-  if s in matchobj.group(0):
+ w=matchobj.group(0).strip()
+ t=w.replace(" ","_")
+ W["NE00W00"].append(t)
+ t=" "+"NE00W00"+" "
+ return t
+def jt(matchobj):
+ for z in j:
+  if z in matchobj.group(0):
    return matchobj.group(0)
- o=matchobj.group(0).strip()
- c=["Messenger","Twitter","Facebook","Internet","Instagram","Google","Skype","Tumblr","Pocket","Telegram","Pinterest","Reddit","Linkedin","Youtube","Síguenos","Inicio"]
- for Q in c:
-  if o.startswith(Q):
+ w=matchobj.group(0).strip()
+ t=w.replace(" ","_")
+ W["NE00S00"].append(t)
+ t=" "+"NE00S00"+" "
+ return t
+def jX(matchobj):
+ for z in j:
+  if z in matchobj.group(0):
    return matchobj.group(0)
- G=o.replace(" ","_")
- G=" "+"NE00U00_"+G+" "
- return G
-def ac(args):
- H=r"\b(el|la|los|las|un|uno|una|unos|unas|lo|al|del)\b"
- l=r"\b(el|la|los|las|lo|al|del)\b"
- t=r"\b(mi|mis|nuestro|nuestros|nuestra|nuestras|tu|tus|vuestro|vuestros|vuestra|vuestras|su|sus)\b"
- E=r"\b(mi|mis|nuestro|nuestros|nuestra|nuestras)\b"
- W=r"\b[Ee]st(oy|ás|á|amos|áis|án|aba|abas|aba|ábamos|abais|aban|uve|uviste|uvo|uvimos|uvisteis|uvieron|aré|arás|ará|aremos|aréis|arán|aría|arías|aríamos|arían|é|és|emos|éis|én|uviera|uvieras|uviéramos|uvierais|uvieran|uviese|uvieses|uviese|uviésemos|uvieseis|uviesen|uviere|uvieres|uviere|uviéremos|uviereis|uvieren)\b"
- J=r"\b[hH](e|as|a|emos|abéis|an|abía|abías|abía|abíamos|abíais|abían|ube|ubiste|ubo|ubimos|ubisteis|ubieron|abré|abrás|abrá|abremos|abréis|abrán|abría|abrías|abríamos|abríais|abrían|aya|ayas|ayamos|ayáis|ayan|ubiera|ubieras|ubiéramos|ubierais|ubieran|ubiese|ubieses|ubiésemos|ubieseis|ubiesen|ubiere|ubieres|ubiéremos|ubiereis|ubieren) +estado\b"
- e=r"\band(o|as|a|amos|áis|an|aba|abas|aba|ábamos|abais|aban|uve|uviste|uvo|uvimos|uvisteis|uvieron|aré|arás|ará|aremos|aréis|arán|aría|arías|aríamos|arían|e|es|emos|éis|en|uviera|uvieras|uviéramos|uvierais|uvieran|uviese|uvieses|uviese|uviésemos|uvieseis|uviesen|uviere|uvieres|uviere|uviéremos|uviereis|uvieren)\b"
- C=r"\b(he|has|ha|hemos|habéis|han|había|habías|había|habíamos|habíais|habían|hube|hubiste|hubo|hubimos|hubisteis|hubieron|habré|habrás|habrá|habremos|habréis|habrán|habría|habrías|habríamos|habríais|habrían|haya|hayas|hayamos|hayáis|hayan|hubiera|hubieras|hubiéramos|hubierais|hubieran|hubiese|hubieses|hubiésemos|hubieseis|hubiesen|hubiere|hubieres|hubiéremos|hubiereis|hubieren) +andado\b"
- z=r"\bv(oy|as|a|amos|ais|an|aya|ayas|ayamos|ayáis|ayan)|\b(i|í)(ba|bas|bamos|bais|ban|ré|rás|rá|remos|réis|rán|ría|rías|ríamos|ríais|rían)|\bfu(i|iste|e|imos|isteis|eron|era|eras|éramos|erais|eran|ese|eses|ésemos|eseis|esen|ere|eres|éremos|ereis|eren)\b"
- I=r"\b(he|has|ha|hemos|habéis|han|había|habías|había|habíamos|habíais|habían|hube|hubiste|hubo|hubimos|hubisteis|hubieron|habré|habrás|habrá|habremos|habréis|habrán|habría|habrías|habríamos|habríais|habrían|haya|hayas|hayamos|hayáis|hayan|hubiera|hubieras|hubiéramos|hubierais|hubieran|hubiese|hubieses|hubiésemos|hubieseis|hubiesen|hubiere|hubieres|hubiéremos|hubiereis|hubieren) +ido\b"
- y=r"\bviaj(o|as|a|amos|áis|an|aba|abas|aba|ábamos|abais|aban|é|aste|ó|amos|asteis|aron|aré|arás|ará|aremos|aréis|arán|aría|arías|aríamos|arían|e|es|emos|éis|en|ara|aras|áramos|arais|aran|ase|ases|ásemos|aseis|asen|are|ares|áremos|areis|aren)\b"
- w=r"\b(he|has|ha|hemos|habéis|han|había|habías|había|habíamos|habíais|habían|hube|hubiste|hubo|hubimos|hubisteis|hubieron|habré|habrás|habrá|habremos|habréis|habrán|habría|habrías|habríamos|habríais|habrían|haya|hayas|hayamos|hayáis|hayan|hubiera|hubieras|hubiéramos|hubierais|hubieran|hubiese|hubieses|hubiésemos|hubieseis|hubiesen|hubiere|hubieres|hubiéremos|hubiereis|hubieren) +viajado\b"
- v=args[1]
- with at(v,"r",encoding="utf-8")as archivo:
-  D=archivo.read()
- r=D
- j=l+r"( +[^A-ZÁÉÍÓÚÑ\W]+ +)*?"
- O=r"(([A-ZÁÉÍÓÚÑ]+[ \b]+)+|([A-ZÁÉÍÓÚÑ][^A-ZÁÉÍÓÚÑ\W]+[ \b]+)+)"
- S=aQ(r"(?<=\b[Ee]n )+("+j+O+"|"+O+")")
- r=S.sub(aG,r)
- Y=aQ(r"("+W+"|"+J+")"+" +entre +[\w ]+")
- B=aQ(r"("+W+"|"+J+")"+" +(cerca|lejos) +de +[\w ]+")
- f=r"[A-HJ-NPR-Z]{3}-?[0-9]{2}-?[0-9]{2}"
- U=r"[A-HJ-NPR-Z]{3}-?[0-9]{3}-?[A-HJ-NPR-Z]"
- L=r"[A-HJ-NPR-Z]-?[0-9]{2,3}-?[A-HJ-NPR-Z]{3}"
+ w=matchobj.group(0).strip()
+ X=["Messenger","Twitter","Facebook","Internet","Instagram","Google","Skype","Tumblr","Pocket","Telegram","Pinterest","Reddit","Linkedin","Youtube","Síguenos","Inicio"]
+ for k in X:
+  if w.startswith(k):
+   return matchobj.group(0)
+ t=w.replace(" ","_")
+ W["NE00U00"].append(t)
+ t=" "+"NE00U00"+" "
+ return t
+def jk(args):
+ n=r"\b(el|la|los|las|un|uno|una|unos|unas|lo|al|del)\b"
+ e=r"\b(el|la|los|las|lo|al|del)\b"
+ x=r"\b(mi|mis|nuestro|nuestros|nuestra|nuestras|tu|tus|vuestro|vuestros|vuestra|vuestras|su|sus)\b"
+ f=r"\b(mi|mis|nuestro|nuestros|nuestra|nuestras)\b"
+ K=r"\b[Ee]st(oy|ás|á|amos|áis|án|aba|abas|aba|ábamos|abais|aban|uve|uviste|uvo|uvimos|uvisteis|uvieron|aré|arás|ará|aremos|aréis|arán|aría|arías|aríamos|arían|é|és|emos|éis|én|uviera|uvieras|uviéramos|uvierais|uvieran|uviese|uvieses|uviese|uviésemos|uvieseis|uviesen|uviere|uvieres|uviere|uviéremos|uviereis|uvieren)\b"
+ Y=r"\b[hH](e|as|a|emos|abéis|an|abía|abías|abía|abíamos|abíais|abían|ube|ubiste|ubo|ubimos|ubisteis|ubieron|abré|abrás|abrá|abremos|abréis|abrán|abría|abrías|abríamos|abríais|abrían|aya|ayas|ayamos|ayáis|ayan|ubiera|ubieras|ubiéramos|ubierais|ubieran|ubiese|ubieses|ubiésemos|ubieseis|ubiesen|ubiere|ubieres|ubiéremos|ubiereis|ubieren) +estado\b"
+ y=r"\band(o|as|a|amos|áis|an|aba|abas|aba|ábamos|abais|aban|uve|uviste|uvo|uvimos|uvisteis|uvieron|aré|arás|ará|aremos|aréis|arán|aría|arías|aríamos|arían|e|es|emos|éis|en|uviera|uvieras|uviéramos|uvierais|uvieran|uviese|uvieses|uviese|uviésemos|uvieseis|uviesen|uviere|uvieres|uviere|uviéremos|uviereis|uvieren)\b"
+ P=r"\b(he|has|ha|hemos|habéis|han|había|habías|había|habíamos|habíais|habían|hube|hubiste|hubo|hubimos|hubisteis|hubieron|habré|habrás|habrá|habremos|habréis|habrán|habría|habrías|habríamos|habríais|habrían|haya|hayas|hayamos|hayáis|hayan|hubiera|hubieras|hubiéramos|hubierais|hubieran|hubiese|hubieses|hubiésemos|hubieseis|hubiesen|hubiere|hubieres|hubiéremos|hubiereis|hubieren) +andado\b"
+ r=r"\bv(oy|as|a|amos|ais|an|aya|ayas|ayamos|ayáis|ayan)|\b(i|í)(ba|bas|bamos|bais|ban|ré|rás|rá|remos|réis|rán|ría|rías|ríamos|ríais|rían)|\bfu(i|iste|e|imos|isteis|eron|era|eras|éramos|erais|eran|ese|eses|ésemos|eseis|esen|ere|eres|éremos|ereis|eren)\b"
+ R=r"\b(he|has|ha|hemos|habéis|han|había|habías|había|habíamos|habíais|habían|hube|hubiste|hubo|hubimos|hubisteis|hubieron|habré|habrás|habrá|habremos|habréis|habrán|habría|habrías|habríamos|habríais|habrían|haya|hayas|hayamos|hayáis|hayan|hubiera|hubieras|hubiéramos|hubierais|hubieran|hubiese|hubieses|hubiésemos|hubieseis|hubiesen|hubiere|hubieres|hubiéremos|hubiereis|hubieren) +ido\b"
+ G=r"\bviaj(o|as|a|amos|áis|an|aba|abas|aba|ábamos|abais|aban|é|aste|ó|amos|asteis|aron|aré|arás|ará|aremos|aréis|arán|aría|arías|aríamos|arían|e|es|emos|éis|en|ara|aras|áramos|arais|aran|ase|ases|ásemos|aseis|asen|are|ares|áremos|areis|aren)\b"
+ a=r"\b(he|has|ha|hemos|habéis|han|había|habías|había|habíamos|habíais|habían|hube|hubiste|hubo|hubimos|hubisteis|hubieron|habré|habrás|habrá|habremos|habréis|habrán|habría|habrías|habríamos|habríais|habrían|haya|hayas|hayamos|hayáis|hayan|hubiera|hubieras|hubiéramos|hubierais|hubieran|hubiese|hubieses|hubiésemos|hubieseis|hubiesen|hubiere|hubieres|hubiéremos|hubiereis|hubieren) +viajado\b"
+ h=args[1]
+ with jf(h,"r",encoding="utf-8")as archivo:
+  S=archivo.read()
+ A=S
+ m=e+r"( +[^A-ZÁÉÍÓÚÑ\W]+ +)*?"
+ p=r"(([A-ZÁÉÍÓÚÑ]+[ \b]+)+|([A-ZÁÉÍÓÚÑ][^A-ZÁÉÍÓÚÑ\W]+[ \b]+)+)"
+ o=jn(r"(?<=\b[Ee]n )+("+m+p+"|"+p+")")
+ A=o.sub(jX,A)
+ C=jn(re_servicios)
+ N=C.finditer(A)
+ A=C.sub(jt,A)
+ u=jn(r"("+K+"|"+Y+")"+" +entre +[\w ]+")
+ T=jn(r"("+K+"|"+Y+")"+" +(cerca|lejos) +de +[\w ]+")
+ U=r"[A-HJ-NPR-Z]{3}-?[0-9]{2}-?[0-9]{2}"
+ g=r"[A-HJ-NPR-Z]{3}-?[0-9]{3}-?[A-HJ-NPR-Z]"
+ I=r"[A-HJ-NPR-Z]-?[0-9]{2,3}-?[A-HJ-NPR-Z]{3}"
  V=r"[0-9]{3}-?[A-HJ-NPR-Z]{3}"
- x=r"[A-HJ-NPR-Z]{2}-?[0-9]{2}-?[0-9]{3}"
- p=r"[0-9]-?[A-HJ-NPR-Z]-?[0-9]{2}-?[A-HJ-NPR-Z]{2}"
- P=r"[0-9]-?[A-HJ-NPR-Z]-?[0-9]-?[A-HJ-NPR-Z]{3}"
- X=aQ(r"\b("+f+"|"+U+"|"+L+"|"+V+"|"+x+"|"+p+"|"+P+")\b")
- r=X.sub(i,r)
- k=aQ(r"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"+r'"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])')
- T=k.finditer(r)
- h=[]
- for n in T:
-  h.append(n.group(0))
- with at("lista_correos.pkl","wb")as f:
-  al(h,f)
- r=k.sub(K,r)
- m=aQ(r"^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$",aH)
- r=m.sub(M,r)
- u=aQ(r"\b(\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4})\b")
- r=u.sub(q,r)
- R=aQ(r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+")
- r=R.sub(A,r)
- F=at("resultadoPre.flg","a+",encoding="utf-8")
- F.write(r)
- F.close()
+ v=r"[A-HJ-NPR-Z]{2}-?[0-9]{2}-?[0-9]{3}"
+ s=r"[0-9]-?[A-HJ-NPR-Z]-?[0-9]{2}-?[A-HJ-NPR-Z]{2}"
+ q=r"[0-9]-?[A-HJ-NPR-Z]-?[0-9]-?[A-HJ-NPR-Z]{3}"
+ Q=jn(r"\b("+U+"|"+g+"|"+I+"|"+V+"|"+v+"|"+s+"|"+q+")\b")
+ A=Q.sub(jW,A)
+ L=jn(r"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"+r'"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])')
+ A=L.sub(H,A)
+ O=jn(r"^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$",je)
+ A=O.sub(b,A)
+ B=jn(r"\b(\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4})\b")
+ A=B.sub(M,A)
+ D=jn(r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+|(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+\.com(\.[a-z]+)?")
+ A=D.sub(jw,A)
+ """
+    Salida
+    """
+ c=jf("resultadoPre.flg","a+",encoding="utf-8")
+ c.write(A)
+ c.close()
+ with jf("diccionarioEtiquetas.pkl","wb")as pickle_dump:
+  jx(W,pickle_dump)
  return 0
 if __name__=='__main__':
  import sys
- sys.exit(ac(sys.argv))
+ sys.exit(jk(sys.argv))
+# Created by pyminifier (https://github.com/liftoff/pyminifier)
+
