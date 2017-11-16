576675c74dcad02bbddc03737defc32d3fce6a6b
GuillermoLvG <guillermo.lvg@gmail.com>
2017-11-16 12:23:53 -0600

    Cambios al release para dejarlo igual que en el CNS

--- release/freeling.py
+++ release/freeling.py
@@ -9,7 +9,7 @@ def V(args):
  I=args[1]
  with g(I,"r",encoding="utf-8")as entrada:
   i=g("resultadoFreeling.flg","a+",encoding="utf-8")
-  K("docker run -i --rm freeling analyzer_client 172.17.0.2:50005",stdin=entrada,stdout=i,shell=P)
+  K("C:\\freeling\\bin\\analyzer_client.exe localhost:50005",stdin=entrada,stdout=i,shell=P)
   i.close()
  return 0
 if __name__=='__main__':
