fc8cdbaa4679879b7f886411868c3909ad31cb40
GuillermoLvG <guillermo.lvg@gmail.com>
2017-11-16 12:23:53 -0600

    Cambios al release para dejarlo igual que en el CNS

--- release/NERC_GIL.bat
+++ release/NERC_GIL.bat
@@ -2,8 +2,8 @@
 echo "%1"
 echo "Corriendo pre.py con texto %1"
 python pre.py "%1"
-echo "Corriendo no_server.py"
-python no_server.py resultadoPre.flg
+echo "Corriendo freeling.py"
+python freeling.py resultadoPre.flg
 echo "Corriendo post.py"
 python post.py resultadoFreeling.flg
 echo "Terminado"
