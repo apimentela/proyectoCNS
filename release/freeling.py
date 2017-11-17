352b21ac5f636b8018e5480f28a8c920a3564610
Pimentel <apimentela@iingen.unam.mx>
2017-11-16 19:29:00 -0600

    Cambios para la segunda entrega, se agrega un segundo parámetro de entrada para definir el archivo de salida, FALTA PROBAR

--- release/freeling.py
+++ release/freeling.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-g=open
-P=True
+
 import subprocess
-K=subprocess.call
-import re
-def V(args):
- I=args[1]
- with g(I,"r",encoding="utf-8")as entrada:
-  i=g("resultadoFreeling.flg","a+",encoding="utf-8")
-  K("C:\\freeling\\bin\\analyzer_client.exe localhost:50005",stdin=entrada,stdout=i,shell=P)
-  i.close()
- return 0
-if __name__=='__main__':
- import sys
- sys.exit(V(sys.argv))
+
+def main(args):
+    s_nombre_archivo = args[1]
+    with open(s_nombre_archivo+"_resultadoPre.flg","r",encoding="utf-8") as entrada:
+        salida = open(s_nombre_archivo+"_resultadoFreeling.flg", "w",encoding="utf-8")
+        subprocess.call("C:\\freeling\\bin\\analyzer_client.exe localhost:50005", stdin=entrada, stdout=salida, shell=True)
+        salida.close()
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
