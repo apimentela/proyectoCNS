8fbbe9a07d8390d35b7d85543547004ba4e93420
GuillermoLvG <guillermo.lvg@gmail.com>
2017-11-22 11:29:36 -0600

    Actualizar rutas absolutas de .bat

--- release/NERC_GIL.bat
+++ release/NERC_GIL.bat
@@ -1,9 +1,9 @@
 @echo off
 echo "%1"
 echo "Corriendo pre.py"
-python pre.py "%1"
+python C:\freeling\bin\pre.py "%1"
 echo "Corriendo freeling.py"
-python freeling.py "%1"
+python C:\freeling\bin\freeling.py "%1"
 echo "Corriendo post.py"
-python post.py "%1" "%2"
+python C:\freeling\bin\post.py "%1" "%2"
 echo "Terminado"
